90112c65524d1a332211b371bc49cdde11f74935
Akindele Festus <akindele.festus@decagon.dev>
2022-01-12 13:12:42 +0100

    Ewallet update

---
+++ Ewallet/Data/Repositories/Interfaces/IWalletRepository.cs
@@ -0,0 +1,18 @@
+﻿using Ewallet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ewallet.Data.Repositories.Interfaces
+{
+    public interface IWalletRepository : ICRUDRepository
+    {
+        Task<List<Wallet>> GetAllWallets();
+        Task<List<Wallet>> GetUserWallets();
+        Task<List<Wallet>> GetUserWalletsByAdmin(string userId);
+        Task<Wallet> GetWalletById(string id);
+        Task<Wallet> GetMainWallet();
+        Task<Wallet> HasWallet();
+    }
+}
